a2221c15fca291167e35decd4e026a74deac3ba3
rohit1391 <2021uee1391@mnit.ac.in>
2024-05-15 16:59:23 +0530

    its a small change

---
+++ app.js
@@ -0,0 +1,6 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+const heading = React.createElement("h1", {}, "Hii React chood");
+    const roomt = ReactDOM.createRoot(document.getElementById("root"));
+    roomt.render(heading);
