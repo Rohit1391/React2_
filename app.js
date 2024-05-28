7d98ffdcf99ede3f7389212b1f5fb3f97fbef368
rohit1391 <2021uee1391@mnit.ac.in>
2024-05-28 19:26:31 +0530

    CSS updated

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
-
 import React from "react";
-import ReactDOM from "react-dom";
-const heading = React.createElement("h1", {}, "Hii React chood");
-    const roomt = ReactDOM.createRoot(document.getElementById("root"));
-    roomt.render(heading);
+import ReactDOM from "react-dom/client";
+import './index.css';
+
+const Header=() => {
+return <div className="Header">
+    <div className="logo-container">
+        <img className="logo" src="https://pickystory.com/wp-content/uploads/2022/08/advantages-of-ecommerce-shopping-cart.png"/>
+    </div>
+    <div className="Nav-items">
+        <ul>
+            <li>Home</li>
+            <li>Cart</li>
+            <li>About us</li>
+            <li>Contact us</li>
+        </ul>
+    </div>
+</div>
+}
+const Body=() => {
+    return (
+        <div className="body"></div>
+        
+    )
+    
+}
+const AppLayout=() =>{
+    return <div className="app">
+        <Header/>
+    </div>
+}
+
+            
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
+    
