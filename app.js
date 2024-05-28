7a5224e7d607d68429ff23b8df7ed6eaf873a441
rohit1391 <2021uee1391@mnit.ac.in>
2024-05-28 23:38:51 +0530

    New

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
+// In index.js or App.js or wherever your main React component is
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 const Header=() => {
 return <div className="Header">
     <div className="logo-container">
         <img className="logo" src="https://pickystory.com/wp-content/uploads/2022/08/advantages-of-ecommerce-shopping-cart.png"/>
     </div>
+    <div className="slogan">
+        <h1>Your electro-hub is here !</h1>
+    </div>
+    <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search your gadgets..."
+        />
+        <button className="search-button">
+          <i className="fas fa-search"></i>
+        </button>
+      </div>
     <div className="Nav-items">
         <ul>
             <li>Home</li>
@@ -16,17 +32,29 @@ return <div className="Header">
         </ul>
     </div>
 </div>
-}
+};
+const Itemscard = () => {
+ return (
+    <div className="item-card">
+        <h3>Computers</h3>
+    </div>
+ );
+};
 const Body=() => {
     return (
-        <div className="body"></div>
-        
-    )
+        <div className="body">
+            <div className="itemscontainer">
+                <Itemscard/>
+            </div>
+        </div>
+
+    );
     
-}
+};
 const AppLayout=() =>{
     return <div className="app">
         <Header/>
+        <Body/>
     </div>
 }
 
